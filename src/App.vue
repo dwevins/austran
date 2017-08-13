07b4123e410a31260551edcda7b84d590d15d1ad
Woody Evins <woody.evins@gmail.com>
2017-08-13 11:52:38 -0500

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        {{library}}
+        <img src="./assets/logo.png">
+        <hello></hello>
+    </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Library from './data/TranslationRepo.js'
+
+export default {
+    name: 'app',
+    components: {
+        Hello
+    },
+    data: () => {
+        return {
+            library: Library.lexicon
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/data/TranslationRepo.js
@@ -0,0 +1,8 @@
+export default {
+    lexicon: [
+        {
+            lang: 'eng',
+            term: 'easy'
+        }
+    ]
+}
